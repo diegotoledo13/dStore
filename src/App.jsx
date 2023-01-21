f4f0635b500c6448ca6d4a04f56580f6675f6085
Diego <105314333+diegokill13@users.noreply.github.com>
2023-01-21 10:10:57 -0300

    dStore upload 0.9

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import { Header } from "./Components/Header/Header";
 import { Section } from "./Components/Section/Section";
-import { Slider } from "./Components/Slider/Slider";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Section />
-      <Slider />
     </div>
   );
 }
